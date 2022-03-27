4f2dfd98e8c0fe9159539c73a9e679d2b4c318e0
Djaysel Pessôa <djaysel@outlook.com>
2022-03-27 14:44:02 -0300

    update 03
    desenvolvendo passo 1

--- JAVACalc/src/br/com/djpess/calculadora.java
+++ JAVACalc/src/br/com/djpess/calculadora.java
@@ -1,17 +1,27 @@
 package br.com.djpess;
 
+import java.util.Scanner;
+
 public class calculadora {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Olá");
+		Scanner input = new Scanner(System.in);
 		var active = true;
 		int cont = 0;
 
 		while(active){
 			try{
+				
 				if(cont == 1){
-					System.out.println("passo um concluido!\n");
+					System.out.println("Informe o primeiro valor:");
+					float valor1 = input.nextFloat();
+					System.out.println("Valor informado: "+ valor1);
+					System.out.println("Informe o segundo valor:");
+					float valor2 = input.nextFloat();
+					System.out.println("Valor informado: "+ valor2);
+					System.out.println("O resultado da soma é: " + (valor1 + valor2));
 					active = false;
 				}else{
 					cont += 1;
@@ -22,6 +32,7 @@ public class calculadora {
 
 			}
 		};
+		input.close();
 	}
 
 }
