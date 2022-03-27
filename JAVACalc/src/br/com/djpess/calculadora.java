4c606a58e9d0c0e02b860373bab98768ddadea5e
Djaysel Pessôa <djaysel@outlook.com>
2022-03-27 14:33:33 -0300

    update02
    iniciando processo

--- JAVACalc/src/br/com/djpess/calculadora.java
+++ JAVACalc/src/br/com/djpess/calculadora.java
@@ -5,6 +5,23 @@ public class calculadora {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Olá");
+		var active = true;
+		int cont = 0;
+
+		while(active){
+			try{
+				if(cont == 1){
+					System.out.println("passo um concluido!\n");
+					active = false;
+				}else{
+					cont += 1;
+				}
+
+			}catch(Exception e){
+				System.out.println("Erro log");
+
+			}
+		};
 	}
 
 }
